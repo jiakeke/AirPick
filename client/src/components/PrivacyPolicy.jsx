8be170ff3fa2262e216ab35218eb20fb0a3fb78d
Runzhou Zhu <runzhouz@metropolia.fi>
2024-09-16 20:46:02 +0300

    add privacy policy modal

---
+++ client/src/components/PrivacyPolicy.jsx
@@ -0,0 +1,45 @@
+export default function PrivacyPolicy() {
+  return (
+    <>
+      <div
+        className="modal fade"
+        id="privacyPolicy"
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="privacyPolicyTitle"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="privacyPolicyTitle">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">...</div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
