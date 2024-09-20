73838ad3079cd32bdb61d7f785a7f99e5780583a
Qingyun Wang <lucas.qingyun@gmail.com>
2024-09-20 23:16:37 +0300

    modify order model

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -12,7 +12,7 @@
       "driver": "222",
       "comments": "Child seat required ",
       "price": 888,
-      "status": "new || accepted || completed || closed",
+      "status": "new || ongoing || completed || cancelled",
       "created": "02.03.2024",
       "modified": "02.03.2024",
       "completed": "02.03.2024",
@@ -59,13 +59,15 @@ const orderSchema = new Schema(
     },
 
     passenger: {
-        type: String,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
         required: true
     },
 
     driver: {
-        type: String,
-        required: true
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: null
     },
 
     comments: {
@@ -79,7 +81,7 @@ const orderSchema = new Schema(
 
     status: {
         type: String,
-        enum: ['new', 'accepted', 'completed', 'closed'],
+        enum: ['new', 'ongoing', 'completed', 'cancelled'],
         default: 'new'
     }
   },
