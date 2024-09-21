f3f0b9b35f74ac06017551258fe5ca52b1ff6798
Qingyun Wang <lucas.qingyun@gmail.com>
2024-09-21 23:27:51 +0300

    update order router

--- server/routes/orderRouter.js
+++ server/routes/orderRouter.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const authenticateToken=require("../middleware/authenticateToken");
 const {
     getAllOrder,
     createOrder,
     getOrderById,
     updateOrder,
     deleteOrder,
+    acceptOrder,
+    cancelOrderByDriver,
+    cancelOrderByPassenger,
+    startOrder,
+    completeOrStopOrder,
 }=require("../controllers/orderController")
 
 router.get('/', getAllOrder);
@@ -13,5 +19,10 @@ router.post('/', createOrder);
 router.get('/:orderId', getOrderById);
 router.put('/:orderId', updateOrder);
 router.delete('/:orderId', deleteOrder);
+router.put('/accept/:orderId', authenticateToken, acceptOrder);
+router.put('/cancel/driver/:orderId', authenticateToken, cancelOrderByDriver);
+router.put('/cancel/passenger/:orderId', authenticateToken, cancelOrderByPassenger);
+router.put('/start/:orderId', authenticateToken, startOrder);
+router.put('/completeorstop/:orderId', authenticateToken, completeOrStopOrder);
 
 module.exports=router;
