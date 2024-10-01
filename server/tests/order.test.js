5dec89f7309b7a19d569012d4bdbe99b972de9cb
Runzhou Zhu <runzhouz@metropolia.fi>
2024-10-01 22:04:12 +0300

    order test

--- server/tests/order.test.js
+++ server/tests/order.test.js
@@ -5,11 +5,29 @@ const api = supertest(app);
 const User = require("../models/userModel");
 const Order = require("../models/orderModel");
 
+const driver = {
+  first_name: "firstNameTest",
+  last_name: "lastNameTest",
+  email: "testemail@email.com",
+  password: "hhdhhdhhd",
+  category: "driver",
+};
+
+const passenger = {
+  first_name: "hhd",
+  last_name: "hhd",
+  email: "hhdhhd@hhd.com",
+  password: "hhdhhdhhd",
+  category: "passenger",
+};
+
+let token = null;
+
 const orders = [
   {
     category: "Pick",
-    departure: "Lentäjäntie 3, 01530 Vantaa",
-    destination: "Lakitie 8 E 7 02770 Espoo",
+    departure: "Pick up departure",
+    destination: "Pick up destination",
     persons: 3,
     luggages: 4,
     flight: "E4448",
@@ -24,9 +42,9 @@ const orders = [
     completed: "02.03.2024",
   },
   {
-    category: "Pick",
-    departure: "Lentäjäntie 3, 01530 Vantaa",
-    destination: "Lakitie 8 E 7 02770 Espoo",
+    category: "Drop",
+    departure: "Drop up departure",
+    destination: "Drop up destination",
     persons: 3,
     luggages: 4,
     flight: "E4448",
@@ -35,26 +53,90 @@ const orders = [
     driver: "222",
     comments: "Child seat required ",
     price: 888,
-    status: "new",
+    status: "ongoing",
+    created: "02.03.2024",
+    modified: "02.03.2024",
+    completed: "02.03.2024",
+  },
+  {
+    category: "Pick",
+    departure: "Pick up departure",
+    destination: "Pick up destination",
+    persons: 3,
+    luggages: 4,
+    flight: "E4448",
+    date: "02.03.2024",
+    passenger: "333",
+    driver: "222",
+    comments: "Child seat required ",
+    price: 888,
+    status: "completed",
+    created: "02.03.2024",
+    modified: "02.03.2024",
+    completed: "02.03.2024",
+  },
+  {
+    category: "Drop",
+    departure: "Drop up departure",
+    destination: "Drop up destination",
+    persons: 3,
+    luggages: 4,
+    flight: "E4448",
+    date: "02.03.2024",
+    passenger: "333",
+    driver: "222",
+    comments: "Child seat required ",
+    price: 888,
+    status: "cancelled",
     created: "02.03.2024",
     modified: "02.03.2024",
     completed: "02.03.2024",
   },
 ];
 
-let token = null;
-
 beforeAll(async () => {
   await User.deleteMany({});
-  const result = await api.post("/api/users/login").send({
-    email: "testemail@email.com",
-    password: "hhdhhdhhd",
-  });
-  token = result.body.user.token;
+  await api.post("/api/users/regist").send(driver);
+  await api.post("/api/users/regist").send(passenger);
 });
 
 afterAll(() => {
   mongoose.connection.close();
 });
 
-describe("givev there are initially some ");
+describe("Order test start", () => {
+  it("should log in the user first", async () => {
+    const result = await api
+      .post("/api/users/login")
+      .send({
+        email: passenger.email,
+        password: passenger.password,
+      })
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    token = result.body.user.token;
+  });
+
+  // Test POST /api/orders
+  it("should create a new order when POST /api/orders is called", async () => {
+    const newOrder = {
+      category: "Pick",
+      departure: "Pick up departure",
+      destination: "Pick up destination",
+      persons: 3,
+      luggages: 4,
+      flight: "E4448",
+      date: "02.03.2024",
+      passenger: "333",
+      driver: "222",
+      comments: "Child seat required ",
+      price: -1,
+    };
+    await api
+      .post("/api/orders")
+      .set("Authorization", "bearer " + token)
+      .send(newOrder)
+      .expect(201);
+  });
+});
